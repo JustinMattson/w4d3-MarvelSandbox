403c358705a7a024e9455cf71629174cab351c99
Justin Mattson <justin.mattson@outlook.com>
2020-05-21 20:18:30 -0600

    filter where !Image NOT found

--- app/Services/CharactersService.js
+++ app/Services/CharactersService.js
@@ -89,7 +89,7 @@ class CharactersService {
 
         rawData = rawData.filter(
           (e) =>
-            e.thumbnail.path ==
+            e.thumbnail.path !=
             "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
         );
         console.log(rawData);
